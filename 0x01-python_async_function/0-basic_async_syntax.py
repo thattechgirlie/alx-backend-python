d00857c41c698ee1b2163e61864f62b1d0ca277f
thattechgirlie <wanji.njoro1@gmail.com>
2024-10-22 22:05:47 +0300

    task corrected'

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""coroutine that delays
-certain amount of time and returns it"""
+"""
+coroutine that delays
+certain amount of time and returns it
+"""
 
 import asyncio
 import random
@@ -9,9 +11,11 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """
     Returns a random float between 0 and max_delay,
-    where max_delay is the upper limit.
+    Args:
+        max_delay: upper limit.
+    Returns:
+        random float
     """
     rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
     return rand
-
